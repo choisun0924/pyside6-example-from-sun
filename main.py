27b980b9fafe4c13c28f99aa17d4a7e9f2a3d1f0
최선 트라이텍 컴퓨터 <choisun@hafen.co.kr>
2022-07-28 11:02:57 +0900

    Window 띄움

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello python')
+import os
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+if __name__ == '__main__':
+    mainApp = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(mainApp.exec())
