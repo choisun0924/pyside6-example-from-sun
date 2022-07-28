ed84c47b690a2690d00cbf71486cb992156b4386
최선 트라이텍 컴퓨터 <choisun@hafen.co.kr>
2022-07-28 11:29:17 +0900

    main.ui / main.py 최종

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 os.system("pyside6-uic ui/main.ui > ui_main.py")
 from ui_main import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.testButton)
+
+    def testButton(self):
+        print('testButton')
 
 if __name__ == '__main__':
     mainApp = QApplication(sys.argv)
