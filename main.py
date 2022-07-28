c168e75cf4480b0b824c3e7216a6431c3295d2ee
최선 트라이텍 컴퓨터 <choisun@hafen.co.kr>
2022-07-28 11:19:28 +0900

    UI 변환코드 추가

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 
+os.system("pyside6-uic ui/main.ui > ui_main.py")
+from ui_main import Ui_MainWindow
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
